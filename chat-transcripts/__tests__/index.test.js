410073f38e1e36049f0777578822e77ba12b263a
Ben-Liao <bliao@serenova.com>
2021-03-04 14:45:59 -0400

    Initail chat-transcript

---
+++ chat-transcripts/__tests__/index.test.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+jest.mock('axios');
+
+global.process.env = {
+  AWS_REGION: 'us-east-1',
+  ENVIRONMENT: 'dev',
+  DOMAIN: 'domain',
+};
+
+const event = { params: { 'tenant-id': 123, 'interaction-id': 456 } };
+
+axios.mockImplementation(() => null);
+
+const { handler } = require('../index');
+
+describe('chat-transcripts', () => {
+  describe('Everything is successful', () => {
+    it('returns when the code runs without any error', async () => {
+      // const result = await handler(event);
+      expect(true).toBeTruthy();
+    });
+
+    describe('Walkthrough', () => {
+      beforeAll(async () => {
+        jest.clearAllMocks();
+        await handler(event);
+      });
+    });
+  });
+});
